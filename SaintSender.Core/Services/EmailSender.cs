338c6e631145fe705d16abd586a9bef7b6029b9f
BrizaMD <late16@gmail.com>
2021-08-17 10:55:35 +0200

    [implemented] service for sending mails

---
+++ SaintSender.Core/Services/EmailSender.cs
@@ -0,0 +1,61 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using SaintSender.Core.Interfaces;
+using SaintSender.Core.Models;
+using System;
+
+namespace SaintSender.Core.Services
+{
+    public class EmailSender : IEmailSender
+    {
+            private readonly EmailConfiguration _emailConfig;
+
+            public EmailSender(EmailConfiguration emailConfig)
+            {
+                _emailConfig = emailConfig;
+            }
+
+            public void SendEmail(Message message)
+            {
+                var emailMessage = CreateEmailMessage(message);
+
+                Send(emailMessage);
+            }
+
+            private MimeMessage CreateEmailMessage(Message message)
+            {
+                var emailMessage = new MimeMessage();
+                emailMessage.From.Add(new MailboxAddress(_emailConfig.SMTPFrom));
+                // emailMessage.To.AddRange(message.To);
+                emailMessage.Subject = message.Subject;
+                emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text) { Text = message.Content };
+
+                return emailMessage;
+            }
+
+            private void Send(MimeMessage mailMessage)
+            {
+                using (var client = new SmtpClient())
+                {
+                    try
+                    {
+                        client.Connect(_emailConfig.SMTPHost, _emailConfig.SMTPPort, true);
+                        client.AuthenticationMechanisms.Remove("XOAUTH2");
+                        client.Authenticate(_emailConfig.SMTPLogin, _emailConfig.SMTPPassword);
+
+                        client.Send(mailMessage);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        throw;
+                    }
+                    finally
+                    {
+                        client.Disconnect(true);
+                        client.Dispose();
+                    }
+                }
+            }
+        }
+    }
