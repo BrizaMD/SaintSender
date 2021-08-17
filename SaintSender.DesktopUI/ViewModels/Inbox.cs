cdd739c02e5b1e882da972934617ee429d21075f
MoniBohm <monic@MONCSI-DESKTOP>
2021-08-17 11:01:27 +0200

    [implemented] scroll inbox feature

---
+++ SaintSender.DesktopUI/ViewModels/Inbox.cs
@@ -0,0 +1,14 @@
+﻿using SaintSender.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaintSender.DesktopUI.ViewModels
+{
+    public class Inbox
+    {
+        private InboxService service = new InboxService();
+    }
+}
--- SaintSender.DesktopUI/Views/SendMail.xaml.cs
+++ SaintSender.DesktopUI/Views/SendMail.xaml.cs
@@ -21,7 +21,7 @@ namespace SaintSender.DesktopUI.Views
     {
         public SendMail()
         {
-            InitializeComponent();
+            //InitializeComponent();
         }
     }
 }
