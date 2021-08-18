889eeafffbdfaf847c4f0e481bdb27bb454c4c96
MoniBohm <monic@MONCSI-DESKTOP>
2021-08-18 18:40:44 +0200

    [refactor] Mail Class

--- SaintSender.Core/Services/Mail.cs
+++ SaintSender.Core/Services/Mail.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SaintSender.Core.Services
 {
@@ -24,10 +20,7 @@ namespace SaintSender.Core.Services
 
         public Mail()
         {
-        }
-        public override string ToString()
-        {
-            return this.Subject + " " + this.From;
+
         }
     }
 }
