f6fcad791506896c9debf3028d67a86232eacc98
MoniBohm <monic@MONCSI-DESKTOP>
2021-08-18 18:42:32 +0200

    [added] ValidationTest

---
+++ SaintSender.Core.Tests/ValidationTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaintSender.Core.Tests
+{
+    [TestFixture]
+    class ValidationTest
+    {
+        [SetUp]
+        public void SetUP() { }
+    }
+}
