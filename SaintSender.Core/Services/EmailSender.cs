1af14c9f31386fd677e4cfa5782c172bcd323df9
MoniBohm <monic@MONCSI-DESKTOP>
2021-08-18 19:05:13 +0200

    [refactor] deleted deadcode from EmailSender

--- SaintSender.Core/Services/EmailSender.cs
+++ SaintSender.Core/Services/EmailSender.cs
@@ -8,55 +8,54 @@ namespace SaintSender.Core.Services
 {
     public class EmailSender : IEmailSender
     {
-            private readonly EmailConfiguration _emailConfig;
+        private readonly EmailConfiguration _emailConfig;
 
-            public EmailSender(EmailConfiguration emailConfig)
+        public EmailSender(EmailConfiguration emailConfig)
+        {
+            _emailConfig = emailConfig;
+        }
+
+        public void SendEmail(Message message)
+        {
+            var emailMessage = CreateEmailMessage(message);
+
+            Send(emailMessage);
+        }
+
+        private MimeMessage CreateEmailMessage(Message message)
+        {
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress(_emailConfig.SMTPFrom));
+            emailMessage.To.Add(new MailboxAddress(message.To));
+            emailMessage.Subject = message.Subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text) { Text = message.Content };
+
+            return emailMessage;
+        }
+
+        private void Send(MimeMessage mailMessage)
+        {
+            using (var client = new SmtpClient())
             {
-                _emailConfig = emailConfig;
-            }
-
-            public void SendEmail(Message message)
-            {
-                var emailMessage = CreateEmailMessage(message);
-
-                Send(emailMessage);
-            }
-
-            private MimeMessage CreateEmailMessage(Message message)
-            {
-                var emailMessage = new MimeMessage();
-                emailMessage.From.Add(new MailboxAddress(_emailConfig.SMTPFrom));
-            // emailMessage.To.AddRange(message.To);
-                emailMessage.To.Add(new MailboxAddress(message.To));
-                emailMessage.Subject = message.Subject;
-                emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text) { Text = message.Content };
-
-                return emailMessage;
-            }
-
-            private void Send(MimeMessage mailMessage)
-            {
-                using (var client = new SmtpClient())
+                try
                 {
-                    try
-                    {
-                        client.Connect(_emailConfig.SMTPHost, _emailConfig.SMTPPort, true);
-                        client.AuthenticationMechanisms.Remove("XOAUTH2");
-                        client.Authenticate(_emailConfig.SMTPLogin, _emailConfig.SMTPPassword);
+                    client.Connect(_emailConfig.SMTPHost, _emailConfig.SMTPPort, true);
+                    client.AuthenticationMechanisms.Remove("XOAUTH2");
+                    client.Authenticate(_emailConfig.SMTPLogin, _emailConfig.SMTPPassword);
 
-                        client.Send(mailMessage);
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex.Message);
-                        throw;
-                    }
-                    finally
-                    {
-                        client.Disconnect(true);
-                        client.Dispose();
-                    }
+                    client.Send(mailMessage);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    throw;
+                }
+                finally
+                {
+                    client.Disconnect(true);
+                    client.Dispose();
                 }
             }
         }
     }
+}
